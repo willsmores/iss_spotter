5dfb95ec5de44826084759298b75d48e06b15c01
Steve Willsmore <scwillsmore@gmail.com>
2022-11-24 19:23:25 -0500

    Implemented flyover times function.

--- iss.js
+++ iss.js
@@ -72,23 +72,24 @@ const fetchISSFlyOverTimes = function(coords, callback) {
   
   // This is the request callback
   request(apiEndpoint, (error, response, body) => {
+
+    // console.log(data.response);
+
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    // if non-200 status, assume server error
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching times. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    
     const data = JSON.parse(body);
-    console.log(data)
-
-    // if (error) {
-    //   callback(error, null);
-    //   return;
-    // }
-    // // if non-200 status, assume server error
-    // if (response.statusCode !== 200) {
-    //   const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
-    //   callback(Error(msg), null);
-    //   return;
-    // }
-
-    // if (!error && data) {
-    //   callback(null, data.ip);
-    // }
+    if (!error && data) {
+      callback(null, data.response);
+    }
   });
 };
 
